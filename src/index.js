f6258324a20d46f82a40c051c293b615f2c21f7c
AhmadAboElzahab-MBLM <aelzahab@mblm.com>
2026-01-07 10:15:17 +0400

    test the cron

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 export default {
   async scheduled(event, env, ctx) {
-    console.log("Cron job running at:", new Date().toISOString());
-
-    // Your cron logic here
-    try {
-      const response = await fetch("https://api.example.com/endpoint");
-      console.log("Success!", await response.text());
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    console.log("Ahmad");
   },
 };
